bec0be515122b7584e0f1eddee0c698df1bc7ade
Soyoung <ksyoung3424@gmail.com>
2023-08-21 02:06:23 +0000

    Build a component using sample data and props and refactor PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,19 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => { // Props allow data to be passed in from a parent component, App, into PhotoListItem.
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" alt="Item image" src={props.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" alt="Profile image" src={props.profile} />
+        <div className="photo-list__user-info">
+          {props.username} <br/>
+          <div className="photo-list__user-location"> {props.city}, {props.country}  </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
