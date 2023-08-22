416f01d426ad3cac7afe81774b5b052dfd1ff8d5
Soyoung <ksyoung3424@gmail.com>
2023-08-22 20:59:23 +0000

    add the favourite icon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,25 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+  const [isFavorited, setIsFavorited] = useState(false); // Initial state: isFavorited is false
+
+  const handleFavClick = () => { //  = setIsFavorited(!isFavorited);
+    if (isFavorited) {
+      setIsFavorited(false);
+    } else {
+      setIsFavorited(true);
+    }
+  };
+
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={handleFavClick}>
+        <FavIcon selected={isFavorited} />
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 // defines the structure for displaying a single photo item with its details
 const PhotoListItem = (props) => { // Props allow data to be passed in from a parent component, App, into PhotoListItem.
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img className="photo-list__image" alt="Item image" src={props.imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" alt="Profile image" src={props.profile} />
