f4403d5831efef6e442e47697ce9dbd57e7761f1
Soyoung <ksyoung3424@gmail.com>
2023-08-22 23:11:58 +0000

    Build PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,64 +5,12 @@ import PhotoList from 'components/PhotoList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
+
 
 const App = () => {
-  return ( // renders PhotoList compoments and passes the sampleDataForPhotoList array as a prop called photoListData.
+  return ( // <PhotoList/> that will render multiple <PhotoListItem/> and PhotoList component is passing in the sample data directly into the PhotoListItem components
     <div className="App">
-      <PhotoList photoList = {sampleDataForPhotoList} />
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,11 +3,64 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
+const sampleDataForPhotoList = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    },
+    user: {
+      id: "1",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "2",
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    },
+    user: {
+      id: "2",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "3",
+    location: {
+      city: "Ottawa",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    },
+    user: {
+      id: "3",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+];
 
-const PhotoList = (props) => {
+const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {props.photoList.map((item) => { // maps over the photoListData prop array received from the parent (App) and generates a list of PhotoListItem components
+      {sampleDataForPhotoList.map((item) => { // maps over the photoListData prop array received from the parent (App) and generates a list of PhotoListItem components
         return (
           <PhotoListItem  // renders PhotoListItem component with the pased-in data
             key={item.id}
