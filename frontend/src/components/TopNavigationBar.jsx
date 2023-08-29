2b5cbef192a92cdddc07b3524bfd3927cb8bccd4
Soyoung <ksyoung3424@gmail.com>
2023-08-29 01:01:12 +0000

    Fix alignment of topics in nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,7 +8,9 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics} />
+      <div className="top-nav-bar__topics">
+        <TopicList topics={props.topics} />
+      </div>
       <FavBadge favPhotoCount={props.favPhotoCount} />
     </div>
   );
